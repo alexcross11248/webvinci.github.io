2be3a773df5d3608167443282a9442550d850d05
Tangkage <603850784@qq.com>
2017-10-17 09:51:36 +0800

    update10.17
    添加app上传百分比

--- js/controllers/gegectrl/update-app.js
+++ js/controllers/gegectrl/update-app.js
@@ -36,23 +36,48 @@ app
 					console.log(fd);
 					$scope.fileCompleted = true;
 					$scope.btnName = '提交中...';
-					$.ajax({
-						url: modelService.rootUrl + 'APKHandler.ashx',
-						type: "POST",
-						async: false,
-						cache: false,
-						processData: false,
-						contentType: false,
-						data: fd,
-						success: function(res) {
-							console.log(res);
+					//					$.ajax({
+					//						url: modelService.rootUrl + 'APKHandler.ashx',
+					//						type: "POST",
+					//						async: false,
+					//						cache: false,
+					//						processData: false,
+					//						contentType: false,
+					//						data: fd,
+					//						success: function(res) {
+					//							console.log(res);
+					//							$scope.subFlag = true;
+					//							$scope.btnName = '发布更新';
+					//						},
+					//						error: function(error) {
+					//							console.log(error);
+					//						}
+					//					});
+					var xhr = new XMLHttpRequest();
+					xhr.onreadystatechange = function() {
+						if(xhr.readyState == 4 && xhr.status == 200) {
+//							alert(xhr.responseText);
+							alert('版本更新成功！');
 							$scope.subFlag = true;
 							$scope.btnName = '发布更新';
-						},
-						error: function(error) {
-							console.log(error);
 						}
-					});
+					}
+					xhr.upload.onprogress = function(evt) {
+						//侦查附件上传情况  
+						//通过事件对象侦查  
+						//该匿名函数表达式大概0.05-0.1秒执行一次  
+						//console.log(evt);  
+						//console.log(evt.loaded);  //已经上传大小情况  
+						//evt.total; 附件总大小  
+						var loaded = evt.loaded;
+						var tot = evt.total;
+						var per = Math.floor(100 * loaded / tot); //已经上传的百分比  
+						var son = document.getElementById('uploadProgress');
+						son.innerHTML = per + "%";
+//						son.style.width = per + "%";
+					}
+					xhr.open("post",  modelService.rootUrl + 'APKHandler.ashx');
+					xhr.send(fd);
 
 				}
 			}
